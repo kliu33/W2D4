f8920b0706acd7293c4fdfcc4e4550ce7d1edb47
kliu33 <kliu33@buffalo.edu>
2022-11-17 13:41:18 -0500

    finish my_select

--- recap_exercise_1/lib/problems.rb
+++ recap_exercise_1/lib/problems.rb
@@ -74,7 +74,8 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-
+        prc ||= Proc.new {|k,v| k == v}
+        out = self.select {|k, v| prc.call(k,v)}
     end
 end
 
